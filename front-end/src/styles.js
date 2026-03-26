92e139c0d6b92ae9a24a46b9a79d79309b1a4043
syed1naqvi <san7576@nyu.edu>
2026-03-25 21:13:06 -0400

    back button component

---
+++ front-end/src/styles.js
@@ -0,0 +1,78 @@
+export const styles = {
+  phoneScreen: {
+    minHeight: "100vh",
+    backgroundColor: "#11111b",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "20px"
+  },
+
+  page: {
+    width: "393px",
+    minHeight: "852px",
+    backgroundColor: "#e9e9e9",
+    color: "black",
+    padding: "20px",
+    boxSizing: "border-box"
+  },
+
+  topRow: {
+    marginBottom: "20px"
+  },
+
+  backButton: {
+    width: "48px",
+    height: "48px",
+    backgroundColor: "black",
+    color: "white",
+    border: "none",
+    fontSize: "28px",
+    cursor: "pointer"
+  },
+
+  bigTitle: {
+    textAlign: "center",
+    fontSize: "40px",
+    marginBottom: "30px"
+  },
+
+  stepsBox: {
+    textAlign: "center",
+    fontSize: "14px",
+    lineHeight: "2.6",
+    marginBottom: "30px"
+  },
+
+  formGroup: {
+    marginBottom: "16px"
+  },
+
+  label: {
+    display: "block",
+    fontSize: "14px",
+    marginBottom: "6px",
+    textTransform: "uppercase"
+  },
+
+  input: {
+    width: "100%",
+    height: "34px",
+    borderRadius: "12px",
+    border: "1px solid black",
+    padding: "8px",
+    boxSizing: "border-box",
+    backgroundColor: "white"
+  },
+
+  mainButton: {
+    width: "100%",
+    backgroundColor: "black",
+    color: "white",
+    border: "none",
+    padding: "18px 12px",
+    fontSize: "16px",
+    marginTop: "18px",
+    cursor: "pointer"
+  }
+};
